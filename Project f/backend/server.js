39e615befc16c9e525a4e6e4e9d2683ad4ace285
Jyotsna <gjyotsna0805@gmail.com>
2026-05-30 21:44:29 +0530

    Update server.js

--- Project f/backend/server.js
+++ Project f/backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://gjyotsna0805_db_user:JyoRev@cluster0.livyx9h.mongodb.net/");
 
 const User = mongoose.model("User", {
   name: String
